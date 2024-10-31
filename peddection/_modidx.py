78da26e8c779e5bbbf5f6ac8aed33e57a3baa623
Abyszero <cwforgach@gmail.com>
2024-10-31 05:13:37 +0000

    Grab paird 2d image

--- peddection/_modidx.py
+++ peddection/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://abyszero-org.github.io',
                 'git_url': 'https://github.com/abyszero-org/peddection',
                 'lib_path': 'peddection'},
-  'syms': {'peddection.core': {'peddection.core.foo': ('core.html#foo', 'peddection/core.py')}}}
+  'syms': { 'peddection.core': {'peddection.core.foo': ('core.html#foo', 'peddection/core.py')},
+            'peddection.kitti.data': {},
+            'peddection.kitti.vis': { 'peddection.kitti.vis.loadVeloAsPcd': ('kitti_explore.html#loadveloaspcd', 'peddection/kitti/vis.py'),
+                                      'peddection.kitti.vis.plotCloud': ('kitti_explore.html#plotcloud', 'peddection/kitti/vis.py')}}}
